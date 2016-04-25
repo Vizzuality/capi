eb71461c8efd2771baf40dfe26138c4340b60e58
Simao Belchior <simaobelchior@gmail.com>
2016-04-25 09:23:08 +0100

    Adding caching to /api/v1/sectors and /api/v1/countries

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::CountriesController < ApiController
   def index
-    @countries = Country.all
+    @countries = Rails.cache.fetch(cache_key, expires_in: 5.days) do
+      Country.all
+    end
     render json: @countries, each_serializer: CountrySerializer
+  end
+
+  private
+
+  def cache_key
+    "countries"
   end
 end
--- app/controllers/api/v1/sectors_controller.rb
+++ app/controllers/api/v1/sectors_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::SectorsController < ApiController
   def index
-    @sectors = Sector.all
+    @sectors = Rails.cache.fetch(cache_key, expires_in: 5.days) do
+      Sector.all
+    end
     render json: @sectors, each_serializer: SectorSerializer
+  end
+
+  private
+
+  def cache_key
+    "sectors"
   end
 end
