be60b6e74807560013eeab78ce3428bbfe72ae05
Simao Belchior <simaobelchior@gmail.com>
2016-04-26 19:18:32 +0200

    Fixes projects endpoint

--- app/models/projects_summary.rb
+++ app/models/projects_summary.rb
@@ -1,5 +1,5 @@
 class ProjectsSummary < CartoDb
-  COLUMNS = [:lat, :lng, :start_date, :end_date, :sectors_slug]
+  COLUMNS = [:lat, :lng, :start_date, :sectors_slug]
 
   attr_reader *COLUMNS
 
@@ -7,7 +7,6 @@ class ProjectsSummary < CartoDb
     @lat = hsh[:lat]
     @lng = hsh[:lng]
     @start_date = hsh[:start_date]
-    @end_date = hsh[:end_date]
     @sectors_slug = hsh[:sectors_slug] && hsh[:sectors_slug].map {|t| "'#{t}'"}.
       join(",")
   end
@@ -23,73 +22,36 @@ class ProjectsSummary < CartoDb
         "name": @results["country"]
       },
       "totals": {
-        "projects": total_projects,
-        "people": total_people,
+        "projects": @results["total_projects"],
+        "people": @results["total_peo"],
         "women_and_girls": @results["w_g_reached"],
-        "men": total_people - @results["w_g_reached"]
+        "men": @results["w_g_reached"] ? @results["total_peo"] - @results["w_g_reached"] : nil
       },
-      "filtered": {
-        "projects": filtered_projects,
-        "people": filtered_people,
-      },
-      "sectors": sectors_from
+      "sectors": sectors_from,
+      "year": @start_date.try(:year) || (Date.today.year-1)
     }
   end
 
   def summary_query
     %Q(
       SELECT projects.country, projects.iso,
+      total_peo, total_projects, w_g_reached,
       #{project_cols.join(", ")},
-      #{people_cols.join(", ")},
-      SUM(w_g_reached) AS w_g_reached
+      #{people_cols.join(", ")}
       FROM #{ProjectsSummary.table_name} AS projects
       INNER JOIN #{Country.table_name} AS countries ON
       countries.iso = projects.iso
       WHERE ST_CONTAINS(countries.the_geom, ST_SetSRID(ST_MakePoint(
       #{@lng}, #{@lat}), 4326))
       #{where_clause}
-      GROUP by projects.country, projects.iso
     )
   end
 
   def where_clause
-    if @start_date && @end_date
-      "AND year BETWEEN #{Date.parse(@start_date).year} AND #{Date.parse(@end_date).year}"
+    if @start_date
+      "AND year = #{Date.parse(@start_date).year}"
     else
-      ""
-    end
-  end
-
-  def total_projects
-    @all_sectors.inject(0) do |sum, sector|
-      sum += @results["#{sector.slug}_projects"]
-    end
-  end
-
-  def filtered_projects
-    @all_sectors.inject(0) do |sum, sector|
-      sum += if !@sectors_slug || @sectors_slug.include?(sector.slug)
-               @results["#{sector.slug}_projects"]
-             else
-               0
-             end
-    end
-  end
-
-  def total_people
-    @all_sectors.inject(0) do |sum, sector|
-      sum += @results["#{sector.slug}_people"]
-    end
-  end
-
-  def filtered_people
-    @all_sectors.inject(0) do |sum, sector|
-      sum += if !@sectors_slug || @sectors_slug.include?(sector.slug)
-               @results["#{sector.slug}_people"]
-             else
-               0
-             end
-
+      "AND year = #{Date.today.year-1}"
     end
   end
 
@@ -112,11 +74,11 @@ class ProjectsSummary < CartoDb
   private
 
   def project_cols
-    @all_sectors.map{|s| "SUM(#{s.slug}_projects) AS #{s.slug}_projects"}
+    @all_sectors.map{|s| "#{s.slug}_projects"}
   end
 
   def people_cols
-    @all_sectors.map{|s| "SUM(#{s.slug}_people) AS #{s.slug}_people"}
+    @all_sectors.map{|s| "#{s.slug}_people"}
   end
 
   def self.table_name
