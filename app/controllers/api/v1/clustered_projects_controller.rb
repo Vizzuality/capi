4e93efda92959091653acc69dc1657bccb5614db
Simao Belchior <simaobelchior@gmail.com>
2016-07-12 11:32:59 +0100

    makes numbers bigger, changes display for easier read

--- app/controllers/api/v1/clustered_projects_controller.rb
+++ app/controllers/api/v1/clustered_projects_controller.rb
@@ -2,122 +2,122 @@ class Api::V1::ClusteredProjectsController < ApiController
   def index
     result = [{
       name: "Latin America and Caribbean",
-      total_people: 688157,
+      total_people: 688_157,
       lat: -2.4601812,
       lng: -59.4140625,
       clustered: true,
       per_sector: [{
         slug: "food",
         color: "#000",
-        people: 5500
+        people: 105_500
       }, {
         slug: "econ",
         color: "#333",
-        people: 3500
+        people: 103_500
       }, {
         slug: "educ",
         color: "#555",
-        people: 2500
+        people: 92_500
       }, {
         slug: "emer",
         color: "#888",
-        people: 1500
+        people: 81_500
       }, {
         slug: "heal",
         color: "#aaa",
-        people: 500
+        people: 75_500
       }]
     }, {
       name: "North America",
-      total_people: 1688157,
+      total_people: 1_688_157,
       lat: 42.0329743,
       lng: -100.1953125,
       clustered: true,
       per_sector: [{
         slug: "food",
         color: "#000",
-        people: 105500
+        people: 805_500
       }, {
         slug: "econ",
         color: "#333",
-        people: 35500
+        people: 635_500
       }, {
         slug: "educ",
         color: "#555",
-        people: 2500
+        people: 502_500
       }, {
         slug: "emer",
         color: "#888",
-        people: 1500
+        people: 401_500
       }, {
         slug: "heal",
         color: "#aaa",
-        people: 500
+        people: 100_500
       }]
     }, {
       name: "Asia",
-      total_people: 10688157,
+      total_people: 10_688_157,
       lat: 32.8426736,
       lng: 96.3281250,
       clustered: true,
       per_sector: [{
         slug: "food",
         color: "#000",
-        people: 1105500
+        people: 8_105_500
       }, {
         slug: "econ",
         color: "#333",
-        people: 95500
+        people: 5_950_500
       }, {
         slug: "educ",
         color: "#555",
-        people: 2500
+        people: 3_992_500
       }, {
         slug: "emer",
         color: "#888",
-        people: 1500
+        people: 991_500
       }, {
         slug: "heal",
         color: "#aaa",
-        people: 500
+        people: 800_000
       }]
     }, {
       name: "Australia",
-      total_people: 188157,
+      total_people: 3_188_157,
       lat: -26.4312281,
       lng: 140.9765625,
       clustered: false,
       per_sector: [{
         slug: "educ",
         color: "#555",
-        people: 22500
+        people: 1_222_500
       }, {
         slug: "emer",
         color: "#888",
-        people: 1500
+        people: 999_500
       }, {
         slug: "heal",
         color: "#aaa",
-        people: 500
+        people: 500_500
       }]
     }, {
       name: "West and South Africa",
-      total_people: 1688157,
+      total_people: 1_688_157,
       lat: 7.7109917,
       lng: 20.0390625,
       clustered: false,
       per_sector: [{
         slug: "econ",
         color: "#000",
-        people: 190500
+        people: 990_500
       }, {
         slug: "educ",
         color: "#333",
-        people: 102500
+        people: 602_500
       }, {
         slug: "emer",
         color: "#888",
-        people: 100500
+        people: 300_500
       }]
     }]
     render json: result
