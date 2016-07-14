70984c0c179f89dd3c37a11350fbe6ce42409908
Simao Belchior <simaobelchior@gmail.com>
2016-07-14 09:53:28 +0100

    re-organizing the code to be more structured and make more sense

---
+++ app/models/projects/clustered.rb
@@ -0,0 +1,37 @@
+class Projects::Clustered < Projects
+
+  SECTORS_LIMIT = 6
+
+  private
+
+  def projects_data_cache_key
+    [
+      "regions_data",
+      @year
+    ].join("-")
+  end
+
+  def projects_query
+    <<-SQL
+      SELECT
+      ST_Y(ST_Centroid(st_union(s.the_geom))) AS lat,
+      ST_X(ST_Centroid(st_union(s.the_geom))) AS lng,
+      s.region_un AS name, p.year,
+      sum(p.clim_people) AS clim,
+      sum(p.econ_people) AS econ,
+      sum(p.educ_people) AS educ,
+      sum(p.emer_people) AS emer,
+      sum(p.food_people) AS food,
+      sum(p.heal_people) AS heal,
+      sum(p.refu_people) AS refu,
+      sum(p.wate_people) AS wate,
+      sum(p.w_g_reached) w_g_reached,
+      sum(p.total_peo) AS total_people,
+      ntile(3) over(order by sum(p.total_peo) desc) AS bucket
+      FROM borders s
+      INNER JOIN projects p ON s.iso=p.iso
+      WHERE year = #{@year.to_i} AND p.total_peo > 0
+      GROUP BY s.region_un, p.year
+    SQL
+  end
+end
