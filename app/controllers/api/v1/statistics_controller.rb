0cfe6f22663281cdf8638a4f4073745c1eecba3b
j8seangel <jseangel@gmail.com>
2017-12-14 16:46:15 +0100

    Include stories count statistic

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::StatisticsController < ApiController
   def index
     @result = DonationsStatistics.new(stats_params).fetch
     @result.merge!(ProjectsStatistics.new(stats_params).fetch)
+    @result.merge!(StoriesStatistics.new().fetch)
     render json: @result
   end
 
---
+++ app/models/stories_statistics.rb
@@ -0,0 +1,36 @@
+class StoriesStatistics < CartoDb
+
+    def fetch
+      results = cached_stats
+      return {} unless results
+      {
+        total_stories: results["total"]
+      }
+    end
+
+    def cached_stats
+      Rails.cache.fetch(stats_cache_key, expires_in: 3.months) do
+        StoriesStatistics.send_query(stats_query)["rows"].try(:first)
+      end
+    end
+
+    def stats_cache_key
+      [
+        "stories-stats",
+        "#{Date.today}"
+      ].join("-")
+    end
+
+    def stats_query
+      %Q(
+        SELECT SUM(total) as total
+        FROM #{StoriesStatistics.table_name}
+      )
+    end
+
+    private
+
+    def self.table_name
+      "output_stories"
+    end
+  end
